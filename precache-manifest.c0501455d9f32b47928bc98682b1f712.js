4fc4c12bf8d60c223c76f63060d7efa4cb54536c
Calvin Ly <lycalvin99@gmail.com>
2019-05-22 17:15:33 -0700

    Updates

---
+++ precache-manifest.c0501455d9f32b47928bc98682b1f712.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b526b6077f2f5ab0428d0c0364de45",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "cb19559e613a0f714f4e",
+    "url": "/todo-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "45ec3c7c874908180eaa",
+    "url": "/todo-list/static/js/2.9e75a866.chunk.js"
+  },
+  {
+    "revision": "cb19559e613a0f714f4e",
+    "url": "/todo-list/static/js/main.e08b3156.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
